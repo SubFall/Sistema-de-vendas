c9c40cf2bcaf71348c7410cc3e935d63b1eb6d35
Thalysom <thalysom.wolfgram@gmail.com>
2026-03-18 20:58:22 -0400

    Refatorando o código

--- src/dev/thalysom/pessoas/domain/ConsoleMenu.java
+++ src/dev/thalysom/pessoas/domain/ConsoleMenu.java
@@ -1,13 +1,11 @@
 package dev.thalysom.pessoas.domain;
 
-import dev.thalysom.util.ConsoleUtils;
-
-import java.time.LocalDate;
 import java.util.Scanner;
 
 public class ConsoleMenu {
     public Scanner scanner = new Scanner(System.in);
     public CadastroPessoa cadastroPessoa = new CadastroPessoa();
+    public PessoaService pessoaService = new PessoaService();
 
     public void iniciar() {
         int opcao;
@@ -17,13 +15,13 @@ public class ConsoleMenu {
 
             switch (opcao) {
                 case 1:
-                    cadastrarPessoa(scanner, cadastroPessoa);
+                    pessoaService.cadastrarPessoa(scanner, cadastroPessoa);
                     break;
                 case 2:
                     cadastroPessoa.listar();
                     break;
                 case 3:
-                    removerPessoa(scanner, cadastroPessoa);
+                    pessoaService.removerPessoa(scanner, cadastroPessoa);
                     break;
                 case 4:
                     System.out.println("Sainda do programa");
@@ -36,78 +34,16 @@ public class ConsoleMenu {
     }
 
     private static void mostrarMenu() {
+        System.out.println("####### Lua.Net #######");
+        System.out.println("=======================");
+        System.out.println("|       Pessoas        |");
+        System.out.println("=======================");
         System.out.println("1 - Cadastrar");
         System.out.println("2 - Listar");
         System.out.println("3 - Remover");
         System.out.println("4 - Sair");
     }
 
-    private static void cadastrarPessoa(Scanner scanner, CadastroPessoa cadastroPessoa) {
-        System.out.println("1 - Pessoa Física");
-        System.out.println("2 - Pessoa Jurídica");
 
-        int op;
-        op = scanner.nextInt();
-        scanner.nextLine();
-
-        switch (op) {
-            case 1:
-                PessoaFisica pessoaFisica = new PessoaFisica();
-
-                System.out.print("Digite seu nome:");
-                pessoaFisica.setNome(scanner.nextLine());
-
-                System.out.print("Digite a data de nascimento (AAAA-MM-DD): ");
-                LocalDate data = ConsoleUtils.lerData(scanner);
-                pessoaFisica.setAnoNascimento(data);
-
-                System.out.print("Digite seu CPF:");
-
-                try {
-                    pessoaFisica.setDocumento(scanner.nextLine());
-                    cadastroPessoa.adicionar(pessoaFisica);
-                } catch (IllegalArgumentException e) {
-                    System.out.println(e.getMessage());
-                }
-
-                break;
-            case 2:
-                PessoaJuridica pessoaJuridica = new PessoaJuridica();
-
-                System.out.print("Digite seu nome:");
-                pessoaJuridica.setNome(scanner.nextLine());
-
-                System.out.print("Digite seu CNPJ:");
-
-                try {
-                    pessoaJuridica.setDocumento(scanner.nextLine());
-                    cadastroPessoa.adicionar(pessoaJuridica);
-                } catch (IllegalArgumentException e) {
-                    System.out.println(e.getMessage());
-                }
-
-                break;
-        }
-
-    }
-
-    private static void removerPessoa(Scanner scanner, CadastroPessoa cadastroPessoa) {
-        System.out.println("1 - Pessoa Física");
-        System.out.println("2 - Pessoa Jurídica");
-
-        int op;
-        op = scanner.nextInt();
-        scanner.nextLine();
-
-        switch (op) {
-            case 1:
-                System.out.print("Digite seu CPF:");
-                cadastroPessoa.remover(scanner.nextLine());
-                break;
-            case 2:
-                System.out.print("Digite seu CNPJ:");
-                cadastroPessoa.remover(scanner.nextLine());
-        }
-    }
 }
 
--- src/dev/thalysom/pessoas/domain/PessoaService.java
+++ src/dev/thalysom/pessoas/domain/PessoaService.java
@@ -1,4 +1,77 @@
 package dev.thalysom.pessoas.domain;
 
+import dev.thalysom.util.ConsoleUtils;
+
+import java.time.LocalDate;
+import java.util.Scanner;
+
 public class PessoaService {
+
+    public void cadastrarPessoa(Scanner scanner, CadastroPessoa cadastroPessoa) {
+        System.out.println("1 - Pessoa Física");
+        System.out.println("2 - Pessoa Jurídica");
+
+        int op;
+        op = scanner.nextInt();
+        scanner.nextLine();
+
+        switch (op) {
+            case 1:
+                PessoaFisica pessoaFisica = new PessoaFisica();
+
+                System.out.print("Digite seu nome:");
+                pessoaFisica.setNome(scanner.nextLine());
+
+                System.out.print("Digite a data de nascimento (AAAA-MM-DD): ");
+                LocalDate data = ConsoleUtils.lerData(scanner);
+                pessoaFisica.setAnoNascimento(data);
+
+                System.out.print("Digite seu CPF:");
+
+                try {
+                    pessoaFisica.setDocumento(scanner.nextLine());
+                    cadastroPessoa.adicionar(pessoaFisica);
+                } catch (IllegalArgumentException e) {
+                    System.out.println(e.getMessage());
+                }
+
+                break;
+            case 2:
+                PessoaJuridica pessoaJuridica = new PessoaJuridica();
+
+                System.out.print("Digite seu nome:");
+                pessoaJuridica.setNome(scanner.nextLine());
+
+                System.out.print("Digite seu CNPJ:");
+
+                try {
+                    pessoaJuridica.setDocumento(scanner.nextLine());
+                    cadastroPessoa.adicionar(pessoaJuridica);
+                } catch (IllegalArgumentException e) {
+                    System.out.println(e.getMessage());
+                }
+
+                break;
+        }
+
+    }
+
+    public void removerPessoa(Scanner scanner, CadastroPessoa cadastroPessoa) {
+        System.out.println("1 - Pessoa Física");
+        System.out.println("2 - Pessoa Jurídica");
+
+        int op;
+        op = scanner.nextInt();
+        scanner.nextLine();
+
+        switch (op) {
+            case 1:
+                System.out.print("Digite seu CPF:");
+                cadastroPessoa.remover(scanner.nextLine());
+                break;
+            case 2:
+                System.out.print("Digite seu CNPJ:");
+                cadastroPessoa.remover(scanner.nextLine());
+        }
+    }
 }
